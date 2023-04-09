ba8fc972824809becc42c89968f2f203f6701ec3
noornabi07 <noornabiprogram07@gmail.com>
2023-04-09 23:27:15 +0600

    home and header route path setup done

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Banner from './components/Banner/Banner';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Banner></Banner>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
