1a884653abef9444486196c5b7626c51777b7f19
noornabi07 <noornabiprogram07@gmail.com>
2023-04-10 23:49:25 +0600

    Rechart make design done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,78 @@
 import React from 'react';
+import {
+    ComposedChart,
+    Line,
+    Area,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+    CartesianAxis,
+  } from 'recharts';
+
 
 const Statistics = () => {
+
+    const assignment = [
+        {
+            name: 'HTML & CSS',
+            get_marks: 60,
+            highest_marks: 60,
+        },
+        {
+            name: 'Responsive Design',
+            get_marks: 60,
+            highest_marks: 60,
+        },
+        {
+            name: 'Project Using Framework',
+            get_marks: 58,
+            highest_marks: 60,
+        },
+        {
+            name: 'First Javascript',
+            get_marks: 60,
+            highest_marks: 60,
+        },
+        {
+            name: 'Integrate Javascript',
+            get_marks: 60,
+            highest_marks: 60,
+        },
+        {
+            name: 'Browser Debug',
+            get_marks: 60,
+            highest_marks: 60,
+        },
+        {
+            name: 'Intermatiade Javascript',
+            get_marks: 58,
+            highest_marks: 60,
+        },
+        {
+            name: 'Simple React',
+            get_marks: 60,
+            highest_marks: 60,
+        },
+    ];
+
+
     return (
-        <div>
-            <h2>This is statistics page</h2>
+        <div className='mt-20 flex justify-center'>
+            <ComposedChart
+                width={900}
+                height={400}
+                data={assignment}
+            >
+            <Tooltip></Tooltip>
+            <XAxis></XAxis>
+            <YAxis></YAxis>
+            <Bar dataKey="highest_marks" barSize={20} fill="#413ea0"></Bar>
+             <Line type="monotone" dataKey="get_marks" stroke="#ff7300" />
+            </ComposedChart>
         </div>
     );
 };
